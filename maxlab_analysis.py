259e3d6886f593229c277ba275bcbb9151dfdb22
mrwun2002 <nathan.wu.nhw5@yale.edu>
2023-06-12 15:13:11 -0400

    pdf cdf

--- maxlab_analysis.py
+++ maxlab_analysis.py
@@ -5,38 +5,12 @@ import pandas as pd
 import time
 
 from scipy.signal import find_peaks
+import scipy.stats as stats
 
 """
 This script shows how to open a raw data file and how to read and interpret the data. 
 ATTENTION: The data file format is not considered stable and may change in the future.
 """
-
-
-def load_from_file(filename, well_no, recording_no, start_frame, block_size):
-    # The maximum allowed block size can be increased if needed,
-    # However, if the block size is too large, handling of the data in Python gets too slow.
-
-    #sample rate is 20000 samples per second
-    max_allowed_block_size = 40000
-    assert(block_size<=max_allowed_block_size)
-    h5_file = h5py.File(filename, 'r')
-    h5_object = h5_file['wells']['well{0:0>3}'.format(well_no)]['rec{0:0>4}'.format(recording_no)]
-
-    # Load settings from file
-    lsb = h5_object['settings']['lsb'][0]
-    sampling = h5_object['settings']['sampling'][0]
-
-    # compute time vector
-    #stop_frame = start_frame+block_size
-    time = np.arange(start_frame,start_frame+block_size) / 20e3
-
-    # Load raw data from file
-    groups = h5_object['groups']
-    print(groups)
-    group0 = groups[next(iter(groups))]
-
-    return group0['raw'][:,start_frame:start_frame+block_size].T * lsb , time
-
 
 
 def load_from_file(filename, well_no, recording_no, start_time, end_time, sample_frequency):
@@ -47,28 +21,28 @@ def load_from_file(filename, well_no, recording_no, start_time, end_time, sample
     # The maximum allowed block size can be increased if needed,
     # However, if the block size is too large, handling of the data in Python gets too slow.
 
-    #sample rate is 20000 samples per second
-    start_frame = round(start_time * 20000)
-    end_frame = round(end_time * 20000)
-    block_size = end_frame - start_frame
-
-
-    frames_per_sample = round(20000/sample_frequency)
-
-    max_allowed_block_size = 40000
-    
-    assert(block_size//frames_per_sample <=max_allowed_block_size)
-
     with h5py.File(filename, "r") as h5_file:
         h5_object = h5_file['wells']['well{0:0>3}'.format(well_no)]['rec{0:0>4}'.format(recording_no)]
 
         # Load settings from file
         lsb = h5_object['settings']['lsb'][0]
-        sampling = h5_object['settings']['sampling'][0]
+        native_sampling_rate = h5_object['settings']['sampling'][0]
+
+        #sample rate is 20000 samples per second
+        start_frame = round(start_time * native_sampling_rate)
+        end_frame = round(end_time * native_sampling_rate)
+        block_size = end_frame - start_frame
+
+
+        frames_per_sample = round(native_sampling_rate/sample_frequency)
+
+        max_allowed_block_size = 40000
+        
+        assert(block_size//frames_per_sample <=max_allowed_block_size)
 
         # compute time vector
         #stop_frame = start_frame+block_size
-        time = np.arange(start_frame,start_frame+block_size, frames_per_sample) / 20e3
+        time = np.arange(start_frame,start_frame+block_size, frames_per_sample) / native_sampling_rate
 
         # Load raw data from file
         groups = h5_object['groups']
@@ -87,6 +61,7 @@ def load_spikes_from_file(filename, well_no, recording_no, voltage_threshold = N
 
         # Load settings from file
         sd_threshold = h5_object['settings']['spike_threshold'][0]
+        native_sampling_rate = h5_object['settings']['sampling'][0]
 
 
         #first_frame_no = h5_object['groups']['routed']['frame_nos'][0]
@@ -103,31 +78,34 @@ def load_spikes_from_file(filename, well_no, recording_no, voltage_threshold = N
 
         if voltage_threshold != None:
             spike_pd_dataset = spike_pd_dataset.loc[spike_pd_dataset['amplitude'].le(voltage_threshold)]
+
+        spike_pd_dataset['frameno'] = spike_pd_dataset['frameno'].multiply(1/native_sampling_rate)
+        
+        spike_pd_dataset.rename(columns = {'frameno':'time'}, inplace = True)
+
         
         return spike_pd_dataset
 
-def find_synchronized_spikes(df: pd.DataFrame, delta_frame = 1000, fraction_threshold = None, threshold_std_multiplier = 4, plot_firing = False): #TODO: make fraction threshold dependent upon distribution of numbers of neurons firing in a certain time delta
+def find_synchronized_spikes(df: pd.DataFrame, delta_t = 0.05, fraction_threshold = None, threshold_std_multiplier = 4, plot_firing = False): #TODO: make fraction threshold dependent upon distribution of numbers of neurons firing in a certain time delta
     '''
     Takes in a pd dataframe of spike data, a percentage threshold for spikes to be considered "synchronized", and a time delta (measured in frames) in which to search for synchronized spikes.
     Returns a pd dataframe containing just the synchronized spikes as well as a dataframe containin the start times of each spike, to the lowest time delta divided by two, and the number of channels active during each spike.
     '''
     num_channels = df['channel'].nunique() #Do we want this to be the number of active channels? the largest channel number?
 
-    max_frameno = max(df['frameno'])
+    max_time = max(df['time'])
 
     synchronized_spikes_df = pd.DataFrame(columns=df.columns)
 
-    #look within a range of framenos, if a spike exists within that range, add the channel to a set of channels. Count the number of elements in that set.
+    #look within a range of times, if a spike exists within that range, add the channel to a set of channels. Count the number of elements in that set.
     
-    #for start_frame in df['frameno'].unique():
-
-    start_frames = np.arange(0, max_frameno, delta_frame//2)
-    fraction_firing_channels = np.zeros_like(start_frames, dtype=float)        
+    start_times = np.arange(0, max_time, delta_t/2)
+    fraction_firing_channels = np.zeros_like(start_times, dtype=float)        
 
 
-    for i, start_frame in enumerate(start_frames):
+    for i, start_time in enumerate(start_times):
 
-        entries_in_range = df.loc[df['frameno'].ge(start_frame) & df['frameno'].lt(start_frame + delta_frame)]
+        entries_in_range = df.loc[df['time'].ge(start_time) & df['time'].lt(start_time + delta_t)]
 
         fraction_firing_channels[i] = (entries_in_range['channel'].nunique())/num_channels
         #if fraction_firing_channels[i] >= fraction_threshold:
@@ -140,20 +118,20 @@ def find_synchronized_spikes(df: pd.DataFrame, delta_frame = 1000, fraction_thre
     
     (spike_indeces, spike_properties) = find_peaks(fraction_firing_channels, height = fraction_threshold)
     #print(spike_indeces)
-    spike_times = start_frames[spike_indeces]
+    spike_times = start_times[spike_indeces]
 
     spike_times_df = pd.DataFrame(list(zip(spike_times, spike_properties['peak_heights'])), columns = ['time', 'fraction channels active'])
 
     if plot_firing:
         plt.figure()
-        plt.plot(start_frames, fraction_firing_channels)
-        plt.xlabel('Frame number')
+        plt.plot(start_times, fraction_firing_channels)
+        plt.xlabel('Time (s)')
         plt.ylabel('Number of channels')
-        plt.hlines(fraction_threshold, 0, max_frameno, 'red')
+        plt.hlines(fraction_threshold, 0, max_time, 'red')
         #plt.show()
 
     for spike_time in spike_times:
-        entries_in_range = df.loc[df['frameno'].ge(spike_time) & df['frameno'].lt(spike_time + delta_frame)]
+        entries_in_range = df.loc[df['time'].ge(spike_time) & df['time'].lt(spike_time + delta_t)]
         synchronized_spikes_df = pd.concat([synchronized_spikes_df, entries_in_range])
 
     return synchronized_spikes_df, spike_times_df
@@ -185,24 +163,79 @@ if __name__ == "__main__":
 
     # Now we need to look at the data with your eyeballs
     plt.figure()
-    plt.scatter(Y['frameno'], Y['channel'], 1)
+    plt.scatter(Y['time'], Y['channel'], 1)
     #plt.scatter(Y_synchronized['frameno'], Y_synchronized['channel'], 1, 'r')
-    plt.xlabel('Frame number')
+    plt.xlabel('Time (s)')
     plt.ylabel('Channels')
     plt.vlines(spike_times['time'], 0, max(Y['channel']), 'red', alpha=0.3)
 
 
     #get differences between spike times
-    spike_diffs = spike_times.diff()
+    spike_diffs = spike_times.diff().dropna()
+    '''
     print(spike_diffs.head())
     plt.figure()
     plt.scatter(spike_diffs['time'], spike_diffs['fraction channels active'])
-
+    plt.xlabel('Time (s)')
+    plt.ylabel('Channels')
+    '''
 
     #histogram of spike diffs
-    plt.hist(spike_diffs['time'])
-    plt.show()
+    num_bins = 20
 
+    plt.figure()
+    plt.subplot(231)
+    plt.hist(spike_diffs['time'], num_bins, density = True)
+    plt.xlabel('Interburst interval (s)')
+    plt.ylabel('Fraction of spikes')
+
+    print(spike_diffs['time'])
+    print(1/spike_diffs['time'])
+
+    plt.subplot(232)
+    plt.hist(1/spike_diffs['time'], num_bins, density = True)
+    plt.xlabel('Spike frequency (1/s)')
+    plt.ylabel('Fraction of spikes')
+
+
+    plt.subplot(233)
+    plt.hist(spike_diffs['time'], num_bins, density = True, cumulative = True)
+    plt.xlabel('t (s)')
+    plt.ylabel('$P(IBI) < t$')
+    
+
+    plt.subplot(234)
+    plt.hist(1/spike_diffs['time'], num_bins, density = True, cumulative = True)
+    plt.xlabel('f (1/s)')
+    plt.ylabel('$P(spike frequency) < f$')
+
+    plt.subplot(235)
+    plt.hist(stats.norm.pdf(spike_diffs['time']), num_bins, density = True)
+    plt.xlabel('t (s)')
+    plt.ylabel('$P(IBI) < t$')
+    
+
+    plt.subplot(236)
+    plt.hist(stats.norm.pdf(1/spike_diffs['time']), num_bins, density = True, cumulative = True)
+    plt.xlabel('f (1/s)')
+    plt.ylabel('$P(spike frequency) < f$')
+
+
+    ###
+    #scipy stats version?
+    IBI_hist = np.histogram(spike_diffs['time'], bins = num_bins)
+    hist_dist = stats.rv_histogram(IBI_hist, density = False)
+    X = np.linspace(0, max(spike_diffs['time']), 100)
+    
+    plt.figure()
+    plt.hist(spike_diffs['time'], num_bins, density = True)
+    plt.plot(X, hist_dist.pdf(X), label= 'PDF')
+    plt.plot(X, hist_dist.cdf(X), label= 'CDF')
+    
+    plt.xlabel('Interburst interval (s)')
+    plt.ylabel('Fraction of spikes')
+
+    plt.show()
     # X,t = load_from_file(filename, 0, 0, 1, 20, 800)
     # print(np.shape(X))
 
